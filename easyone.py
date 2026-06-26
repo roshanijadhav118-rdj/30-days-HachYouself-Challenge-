48dab2910aea320f688ffac99b0e04b911ec45df
TechWarrior <roshnijadhav118@gmail.com>
2026-06-26 13:38:45 +0530

    Add basic age and name input functionalityage calculations

---
+++ easyone.py
@@ -0,0 +1,7 @@
+name=input("Enter name")
+age=int(input("Enter age")
+        
+future_age= age+5
+print(f"hello{name}")
+print(f"your current age is {age}")
+print(f"after 5 years age is {future_age}")        
