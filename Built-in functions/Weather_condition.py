442cc5d2ded99822d2dc89a222593e43ad34eff0
TechWarrior <roshnijadhav118@gmail.com>
2026-07-22 22:24:14 +0530

    Add Weather_condition.py to analyze temperature data

---
+++ Built-in functions/Weather_condition.py
@@ -0,0 +1,8 @@
+temp= [32.5, 35.2, 29.8, 31.4, 36.1, 33.7, 30.5]
+print("hot temp=",max(temp))
+print("cold temp=",min(temp))
+avg=(sum(temp))/len(temp)
+print("rounded=",round(avg,2))
+print("ascending=",sorted(temp))
+print("lenght=",len(temp))
+
